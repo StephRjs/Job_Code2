0651a3c2f71d959b4b09ec7f326ae15e6989d076
Stephanie Rojas <stephanie.rojas@ucrso.info>
2017-05-23 14:31:18 -0600

    Start

--- app/src/main/java/com/example/usuario/job_code/Publish.java
+++ app/src/main/java/com/example/usuario/job_code/Publish.java
@@ -5,21 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class Publish extends AppCompatActivity {
     Button next;
-
+private RadioButton option1, option2, option3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
 
-        next= (Button)findViewById(R.id.button4);
+        next= (Button)findViewById(R.id.btNext);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                option1 = (RadioButton) findViewById(R.id.internship);
+                if(option1.isChecked()){
                 Intent next = new Intent(Publish.this, internship.class);
                 startActivity(next);
+            }else{
+                  option2= (RadioButton) findViewById(R.id.service);
+                    if(option2.isChecked()){
+                        Intent next = new Intent(Publish.this, Service.class);
+                        startActivity(next);
+                    }
+                        option3=(RadioButton) findViewById(R.id.project);
+                        if(option3.isChecked()){
+                            Intent next = new Intent(Publish.this, Project.class);
+                            startActivity(next);
+                        }
+
+                }
             }
         });
     }
--- app/src/main/java/com/example/usuario/job_code/internship.java
+++ app/src/main/java/com/example/usuario/job_code/internship.java
@@ -1,13 +1,68 @@
 package com.example.usuario.job_code;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class internship extends AppCompatActivity {
+    Button post;
+    EditText companyName, description, dueDate, position, contact;
+    URL url =null;
+    HttpURLConnection client= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_internship);
+        post = (Button) findViewById(R.id.btPost);
+        companyName = (EditText) findViewById(R.id.txtCompanyName);
+        description = (EditText) findViewById(R.id.ed_txDescription);
+        dueDate = (EditText) findViewById(R.id.ed_txDueDate);
+        position = (EditText) findViewById(R.id.ed_txPosition);
+        contact = (EditText) findViewById(R.id.ed_txEmail);
+        post = (Button) findViewById(R.id.btPost);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPost();
+                Intent next = new Intent(internship.this, Publish.class);
+                startActivity(next);
+            }
+        });
+    }
+
+
+    public void sendPost(){
+
+        try{
+            System.out.println("entro en el try");
+            System.out.println(this.dueDate+"   " +this.companyName);
+            url = new URL("http://jobcode.azurewebsites.net/Post.svc/addPost?companyName="+this.companyName+
+                    "&description="+this.description+"&email="+this.contact+"&dueDate="+this.dueDate+"&IDTypePost=1&Place_Position_SoftType="+this.position);
+            client = (HttpURLConnection)url.openConnection();
+
+            client.setDoOutput(true);
+            OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+            outputPost.flush();
+            outputPost.close();
+
+        }catch (Exception e){
+            System.out.println("cayo en el cath");
+            System.out.println(e);
+        }
+        finally {
+            if(client != null)
+                client.disconnect();
+        }
     }
 }
